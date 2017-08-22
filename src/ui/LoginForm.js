17d065a930cdd3643a7c81bee3abc3436d8b4a87
Developer Deck 101 <devdeck101@Brunos-MacBook-Pro-2.local>
2017-08-22 18:38:41 -0300

    Tela de registro de usuário - https://youtu.be/t-TJOb6SRoI

--- src/ui/LoginForm.js
+++ src/ui/LoginForm.js
@@ -3,19 +3,80 @@ import React, { Component } from 'react';
 
 class LoginForm extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSignUpSubmit = this.handleSignUpSubmit.bind(this);
         this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
 
         this.state = {
+            username: undefined,
             email: undefined,
-            password: undefined
+            password: undefined,
+            signUp: {
+                success: undefined,
+                message: undefined
+            }
         }
     }
 
     static displayName = 'ui-LoginForm'
+
+    /*
+    Register Form area
+    */
+
+    handleSignUpSubmit(e) {
+        e.preventDefault();
+        let dataToSend = {
+            userData: {
+                username: this.refs.username.value,
+                email: this.refs.email.value,
+                password: this.refs.password.value
+            }
+        };
+        console.log(JSON.stringify(dataToSend))
+
+        let url = 'http://localhost:3001/users/register';
+
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify(dataToSend),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(response => response.json())
+            .then(responseJson => {
+                console.log(responseJson)
+                if (responseJson.success) {
+                    this.setState({
+                        signUp: {
+                            success: true,
+                            message: responseJson.message
+                        }
+                    });
+                } else {
+                    this.setState({
+                        signUp: {
+                            success: false,
+                            message: responseJson.message
+                        }
+                    });
+                }
+            })
+
+        this.refs.username.value = '';
+        this.refs.email.value = '';
+        this.refs.password.value = '';
+
+    }
+
+
+
+    /*
+    Login Form area
+    */
 
     handleSubmit(e) {
         e.preventDefault();
@@ -32,23 +93,23 @@ class LoginForm extends Component {
             method: "POST",
             body: JSON.stringify(dataToSend),
             headers: {
-              "Content-Type": "application/json"
+                "Content-Type": "application/json"
             }
-          }).then(response => response.json())
-          .then(responseJson => {
-              if(responseJson.success){
-                  localStorage.setItem('DD101_TOKEN', responseJson.token);
-              }
-          })
+        }).then(response => response.json())
+            .then(responseJson => {
+                if (responseJson.success) {
+                    localStorage.setItem('DD101_TOKEN', responseJson.token);
+                }
+            })
     }
 
-    handleEmailChange(e){
+    handleEmailChange(e) {
         this.setState({
             email: e.target.value
         });
     }
 
-    handlePasswordChange(e){
+    handlePasswordChange(e) {
         this.setState({
             password: e.target.value
         });
@@ -68,32 +129,46 @@ class LoginForm extends Component {
                                 </button>
                             </div>
                             <div className="modal-body">
-                                <form>
+
+                                {
+                                    this.state.signUp.success !== undefined ? (
+                                        this.state.signUp.success === true ?
+                                            <div className="alert alert-success" role="alert">
+                                                {this.state.signUp.message}
+                                            </div>
+                                            :
+                                            <div className="alert alert-danger" role="alert">
+                                                {this.state.signUp.message}
+                                            </div>
+                                    ) : ''
+                                }
+
+                                <form onSubmit={this.handleSignUpSubmit}>
                                     <div className="form-group">
                                         <label htmlFor="recipient-name" className="form-control-label">Name</label>
-                                        <input type="text" className="form-control" id="username" />
+                                        <input type="text" ref="username" className="form-control" id="username" />
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="email" className="form-control-label">Email</label>
-                                        <input type="email" className="form-control" id="email" />
+                                        <input type="email" ref="email" className="form-control" id="email" />
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="message-text" className="form-control-label">Password:</label>
-                                        <input type="password" className="form-control" id="password" />
+                                        <input type="password" ref="password" className="form-control" id="password" />
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="submit" className="btn btn-primary">Register</button>
                                     </div>
                                 </form>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" className="btn btn-primary">Register</button>
                             </div>
                         </div>
                     </div>
                 </div>
                 {/* Begin Modal Register Form */}
-    
+
                 {/* Begin Modal List Authenticad List  */}
-    
+
                 <div className="modal fade" id="authenticatedModal" tabIndex="-1" role="dialog" aria-labelledby="authenticatedModalLabel" aria-hidden="true">
                     <div className="modal-dialog" role="document">
                         <div className="modal-content">
@@ -101,7 +176,7 @@ class LoginForm extends Component {
                                 <h5 className="modal-title" id="authenticatedModalLabel">Only Authenticated Users can see this list!</h5>
                             </div>
                             <div className="modal-body">
-    
+
                                 <div className="list-group">
                                     <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
                                         <div className="d-flex w-100 justify-content-between">
@@ -119,7 +194,7 @@ class LoginForm extends Component {
                                         <p className="mb-1">E-mail: devdeck101@gmail.com</p>
                                         <small>Password (Should never do that)</small>
                                     </a>
-                                   <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                                    <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
                                         <div className="d-flex w-100 justify-content-between">
                                             <h5 className="mb-1">Name: Developer Deck 101</h5>
                                             <small>01/09/2017</small>
@@ -128,19 +203,19 @@ class LoginForm extends Component {
                                         <small>Password (Should never do that)</small>
                                     </a>
                                 </div>
-    
+
                             </div>
                             <div className="modal-footer">
                                 <button type="button" className="btn btn-secondary btn-block" data-dismiss="modal">Close</button>
-                                
+
                             </div>
                         </div>
                     </div>
                 </div>
-    
+
                 {/* End Modal List Authenticad List */}
-    
-    
+
+
                 {/* Begin Login Form */}
                 <div className="row" style={{ paddingTop: '50px' }}>
                     <div className="col">
@@ -171,11 +246,11 @@ class LoginForm extends Component {
                                     <button type="button" className="btn btn-primary btn-block" data-toggle="modal" data-target="#authenticatedModal" data-whatever="@mdo" >Call Authenticated only API</button>
                                     <small id="emailHelp" className="form-text text-muted">Only registered and logged users can call and see the list. Plese click the button above to call the API.</small>
                                 </form>
-    
-    
+
+
                             </div>
                         </div>
-    
+
                     </div>
                     <div className="col">
                     </div>
